fe9e0ea3af0c21bf617483316bbab1a8d27cf4d1
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2025-11-06 18:55:25 -0600

    Update vite.config.js

--- Stylish_Beauty_Glory/Stylish_Beauty_Glory/vite.config.js
+++ Stylish_Beauty_Glory/Stylish_Beauty_Glory/vite.config.js
@@ -3,13 +3,23 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
+  build: {
+    outDir: 'dist', 
+  },
   server: {
+    port: 5173,
+    open: true,
     proxy: {
-      "/api": {
-        target: "https://stylish-8dn8.vercel.app",
+      '/api': {
+        target: 'https://stylish-8dn8.vercel.app',
         changeOrigin: true,
         secure: true,
       },
     },
   },
-});
+  preview: {
+    port: 4173,
+    open: true,
+  },
+})
