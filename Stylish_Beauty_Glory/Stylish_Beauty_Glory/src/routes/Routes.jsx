33a8d5d77b92030ad75270024e7cd9b6daf8fd26
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2025-11-06 18:39:16 -0600

    Update Routes.jsx

--- Stylish_Beauty_Glory/Stylish_Beauty_Glory/src/routes/Routes.jsx
+++ Stylish_Beauty_Glory/Stylish_Beauty_Glory/src/routes/Routes.jsx
@@ -1,8 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/App";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 
+// --- ADMIN ---
 import AdminLayout from "../layouts/AdminLayout";
 import ManageRoles from "../pages/admin/manageRoles";
 import ManageAdmins from "../pages/admin/manageAdmins";
@@ -11,18 +12,23 @@ import ManageSuppliers from "../pages/admin/manageSuppliers";
 import ManageAccountsPayable from "../pages/admin/manageAccountsPayable";
 import ManageProfileAdmin from "../pages/admin/manageProfile";
 
+// --- CLIENTE ---
 import ClientLayout from "../layouts/ClientLayout";
 import ManageMyDetails from "../pages/client/manageProfile";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* 🔹 Páginas públicas */}
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
 
-      {/* Sección admin */}
+      {/* 🔹 Sección de administración */}
       <Route path="/admin" element={<AdminLayout />}>
+        {/* Redirección automática si solo entra a /admin */}
+        <Route index element={<Navigate to="admins" replace />} />
+
         <Route path="roles" element={<ManageRoles />} />
         <Route path="admins" element={<ManageAdmins />} />
         <Route path="users" element={<ManageUsers />} />
@@ -31,13 +37,16 @@ export default function AppRoutes() {
         <Route path="profile" element={<ManageProfileAdmin />} />
       </Route>
 
-
-      {/* Sección Clientes */}
+      {/* 🔹 Sección de clientes */}
       <Route path="/client" element={<ClientLayout />}>
+        {/* Redirección automática si solo entra a /client */}
+        <Route index element={<Navigate to="profile" replace />} />
+
         <Route path="profile" element={<ManageMyDetails />} />
       </Route>
 
+      {/* 🔹 Si la ruta no existe */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
-
